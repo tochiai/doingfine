2c52e5c9536cf19702cf2e17ec60f1ec2f65a446
Andrew Zey <andrew@andrewzey.com>
2014-08-02 07:12:42 -0700

    (feat) Activate timepicker on schedule state. Toggle confirmation screen on submit

--- client/app/schedule/schedule.controller.js
+++ client/app/schedule/schedule.controller.js
@@ -2,5 +2,41 @@
 
 angular.module('doingFineApp')
   .controller('ScheduleCtrl', function ($scope) {
-    $scope.message = 'Hello';
+    //boolean used to toggle schedule setup and confirmation screens
+    $scope.submitted = false;
+
+    $scope.submit = function(){
+      $scope.submitted = true;
+    };
+
+
+    $scope.mytime = new Date();
+
+    $scope.hstep = 1;
+    $scope.mstep = 1;
+
+    $scope.options = {
+      hstep: [1, 2, 3],
+      mstep: [1, 5, 10, 15, 25, 30]
+    };
+
+    $scope.ismeridian = true;
+    $scope.toggleMode = function() {
+      $scope.ismeridian = ! $scope.ismeridian;
+    };
+
+    $scope.update = function() {
+      var d = new Date();
+      d.setHours( 14 );
+      d.setMinutes( 0 );
+      $scope.mytime = d;
+    };
+
+    $scope.changed = function () {
+      console.log('Time changed to: ' + $scope.mytime);
+    };
+
+    $scope.clear = function() {
+      $scope.mytime = null;
+    };
   });
