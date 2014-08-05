998c69400f0c1cfae70cdfeb99cbdddea34dfc95
Nelson Riley <nelsonriley@gmail.com>
2014-08-04 19:31:25 -0700

    (fix) Tab to space formatting

--- server/components/cron/cron.js
+++ server/components/cron/cron.js
@@ -7,47 +7,47 @@ moment().format();
 
 // get Day as 0-6 and Time as '13:15' for Now (used to search schedules, then send checkins)
 var getDayTime = module.exports.getDayTime = function() {
-	var date = {};
-	var now = moment();
-	var hour = now.hour().toString();
-	if (hour.length === 1) {
-		hour = '0' + hour;
-	}
-	var minute = now.minute().toString();
-	if (minute.length === 1) {
-		minute = '0' + minute;
-	}
-	date.day = now.day();
-	date.time = hour + ':' + minute;
-	return date;
+  var date = {};
+  var now = moment();
+  var hour = now.hour().toString();
+  if (hour.length === 1) {
+    hour = '0' + hour;
+  }
+  var minute = now.minute().toString();
+  if (minute.length === 1) {
+    minute = '0' + minute;
+  }
+  date.day = now.day();
+  date.time = hour + ':' + minute;
+  return date;
 };
 
 // send checkin updates to subscriber of each schedule passed in
 var sendCheckins = module.exports.sendCheckins = function(err, schedules) {
-	if(err) { throw err; };
-	var message = ' wants to know you are OK. Please check in by replying with any text message.';
-	_.forEach(schedules, function(schedule) {
-		twilio.sendText(schedule.publisherPhone, schedule.subscriberName + message);
-	});
+  if(err) { throw err; };
+  var message = ' wants to know you are OK. Please check in by replying with any text message.';
+  _.forEach(schedules, function(schedule) {
+    twilio.sendText(schedule.publisherPhone, schedule.subscriberName + message);
+  });
 };
 
 // find all checkins scheduled for now
 var checkinsMatchingNow = module.exports.checkinsMatchingNow = function(cb) {
-	var now = getDayTime();
-	// find all schedules with current day and time
-	var query = {
-		times: { $in: [now.time] },
-		days: { $in: [now.day] }
-	};
+  var now = getDayTime();
+  // find all schedules with current day and time
+  var query = {
+    times: { $in: [now.time] },
+    days: { $in: [now.day] }
+  };
   Schedule.find(query, cb);
 };
 
 // kick off cron jobs
 module.exports.start = function() {
-	var CronJob = cron.CronJob;
-	// run every minute
-	new CronJob('00 */1 * * * *', function(){
-		checkinsMatchingNow(sendCheckins);
-	}, null, true, 'America/Los_Angeles');
+  var CronJob = cron.CronJob;
+  // run every minute
+  new CronJob('00 */1 * * * *', function(){
+    checkinsMatchingNow(sendCheckins);
+  }, null, true, 'America/Los_Angeles');
 };
 
--- server/components/cron/cron.spec.js
+++ server/components/cron/cron.spec.js
@@ -10,35 +10,36 @@ describe('Cron functionality', function() {
 
   it('should send a checkin reminder text to a nelson\'s phone', function() {
 
-  	// texts cost money, use this only when you need to :)
-  	var dummySchedules = [ {publisherPhone: '+18027936146', subscriberName: 'Nelson Riley' } ];
-  	// cron.sendCheckins(null, dummySchedules);
+    // texts cost money, use this only when you need to :)
+    var dummySchedules = [ {publisherPhone: '+18027936146', subscriberName: 'Nelson Riley' } ];
+    // cron.sendCheckins(null, dummySchedules);
 
   });
 
   it('should successfully query for checkins scheduled for now', function(done) {
 
-  	var now = cron.getDayTime();
+    var now = cron.getDayTime();
 
-  	// create a schedule with current time and day of week then find it using cron
-  	Schedule.find({}).remove(function() {
-  		// create
-		  Schedule.create({
-		    days : [now.day],
-		    times : [now.time]
-		  }, function(err, schedule) {
-			  	// created
-			  	schedule.should.be.type('object');
-			  	// query
-			  	cron.checkinsMatchingNow(function(err, schedules) {
-						// find
-						schedules.should.have.length(1);
-					});
-				}
-		  );
-		});
+    // create a schedule with current time and day of week then find it using cron
+    // wipe
+    Schedule.find({}).remove(function() {
+      // create
+      Schedule.create({
+        days : [now.day],
+        times : [now.time]
+      }, function(err, schedule) {
+          // created
+          schedule.should.be.type('object');
+          // query
+          cron.checkinsMatchingNow(function(err, schedules) {
+            // find
+            schedules.should.have.length(1);
+          });
+        }
+      );
+    });
 
-  	done();
+    done();
     
   });
 
