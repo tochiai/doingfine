3a464c1fd62077ad1460fd12dfdd90f0904e2589
Tyler Ochiai <tyler.a.ochiai@gmail.com>
2014-08-05 18:52:03 -0700

    (feat) Load schedules array into schedules screen

--- server/config/seed.js
+++ server/config/seed.js
@@ -35,7 +35,8 @@ User.find({}).remove(function() {
     provider: 'local',
     name: 'Test User',
     email: 'test@test.com',
-    password: 'test'
+    password: 'test',
+    phone: '+15555551221'
   }, {
     provider: 'local',
     role: 'admin',
