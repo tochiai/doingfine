14c2c35dac76b25ec3532d2a99863a125d289625
Andrew Zey <andrew@andrewzey.com>
2014-08-06 20:06:58 -0700

    (feat) add day formatting

--- client/app/schedules/schedules.controller.js
+++ client/app/schedules/schedules.controller.js
@@ -1,7 +1,19 @@
 'use strict';
 
 angular.module('doingFineApp')
-  .controller('SchedulesCtrl', function ($scope, $http, User) {
+  .controller('SchedulesCtrl', function ($scope, $state, $http, User) {
+
+    $scope.formatDay = function(day) {
+      var daysLong = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+      var daysShort = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+      return daysShort[day];
+    };
+
+    $scope.edit = function(schedule){
+      console.log(schedule);
+      $state.go('edit-schedule', {schedule: 'test'});
+    };
+
     User.get(function(result){
       $scope.userID = result._id;
       $http.get('api/users/' + $scope.userID + '/schedules')
