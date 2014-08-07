eafd1f9bb2475320ae97d5493f8e52e6225863fe
Andrew Zey <andrew@andrewzey.com>
2014-08-06 20:34:47 -0700

    (feat) set tel formatting

--- client/app/edit-schedule/edit-schedule.controller.js
+++ client/app/edit-schedule/edit-schedule.controller.js
@@ -2,78 +2,79 @@
 
 angular.module('doingFineApp')
   .controller('EditScheduleCtrl', function ($scope, $rootScope, $state) {
+
     if (!$rootScope.editSchedule) {
       $state.go('schedules');
     }
-    console.log($rootScope.editSchedule);
-    $scope.schedule = $rootScope.editSchedule;
 
-    //TIME PICKER
-    var time = $scope.schedule.times[0].split(':');
+    else {
 
-    //grab hours from $rootScope
-    var hours = parseInt(time[0]);
-    var minutes = parseInt(time[1]);
-    //grab minutes from $rootScope
+      $scope.schedule = $rootScope.editSchedule;
 
-    //24:59 --> native JS datestamp
+      //PHONE FORMATTING
+      $scope.schedule.publisherPhone = $scope.schedule.publisherPhone.slice(2);
 
-    $scope.schedule.times = new Date();
+      //TIME PICKER
+      var time = $scope.schedule.times[0].split(':');
+      var hours = parseInt(time[0]);
+      var minutes = parseInt(time[1]);
 
-    $scope.schedule.times.setHours(hours); //cannot be prefixed with zero
-    $scope.schedule.times.setMinutes(minutes);
+      $scope.schedule.times = new Date();
+      $scope.schedule.times.setHours(hours); //cannot be prefixed with zero
+      $scope.schedule.times.setMinutes(minutes);
 
 
-    $scope.hstep = 1;
-    $scope.mstep = 1;
+      $scope.hstep = 1;
+      $scope.mstep = 1;
 
-    $scope.options = {
-      hstep: [1, 2, 3],
-      mstep: [1, 5, 10, 15, 25, 30]
-    };
+      $scope.options = {
+        hstep: [1, 2, 3],
+        mstep: [1, 5, 10, 15, 25, 30]
+      };
 
-    $scope.ismeridian = true;
-    $scope.toggleMode = function() {
-      $scope.ismeridian = ! $scope.ismeridian;
-    };
+      $scope.ismeridian = true;
+      $scope.toggleMode = function() {
+        $scope.ismeridian = ! $scope.ismeridian;
+      };
 
-    $scope.update = function() {
-      var d = new Date();
-      d.setHours( 14 );
-      d.setMinutes( 0 );
-      $scope.schedule.times = d;
-    };
+      $scope.update = function() {
+        var d = new Date();
+        d.setHours( 14 );
+        d.setMinutes( 0 );
+        $scope.schedule.times = d;
+      };
 
-    $scope.clear = function() {
-      $scope.schedule.times = null;
-    };
+      $scope.clear = function() {
+        $scope.schedule.times = null;
+      };
 
-    //DAY PICKER
+      //DAY PICKER
 
-    $scope.days = [
-      {name: 'Monday', value: 1},
-      {name: 'Tuesday', value: 2},
-      {name: 'Wednesday', value: 3},
-      {name: 'Thursday', value: 4},
-      {name: 'Friday', value: 5},
-      {name: 'Saturday', value: 6},
-      {name: 'Sunday', value: 0}
-    ];
+      $scope.days = [
+        {name: 'Monday', value: 1},
+        {name: 'Tuesday', value: 2},
+        {name: 'Wednesday', value: 3},
+        {name: 'Thursday', value: 4},
+        {name: 'Friday', value: 5},
+        {name: 'Saturday', value: 6},
+        {name: 'Sunday', value: 0}
+      ];
 
-    $scope.schedule.days = [];
+      $scope.schedule.days = [];
 
-    // toggle selection for a given day by name
-    $scope.toggleDay = function toggleDays(day) {
-      var idx = $scope.schedule.days.indexOf(day);
+      // toggle selection for a given day by name
+      $scope.toggleDay = function toggleDays(day) {
+        var idx = $scope.schedule.days.indexOf(day);
 
-      // is currently selected
-      if (idx > -1) {
-        $scope.schedule.days.splice(idx, 1);
-      }
+        // is currently selected
+        if (idx > -1) {
+          $scope.schedule.days.splice(idx, 1);
+        }
 
-      // is newly selected
-      else {
-        $scope.schedule.days.push(day.value);
-      }
-    };
+        // is newly selected
+        else {
+          $scope.schedule.days.push(day.value);
+        }
+      };
+    }
   });
