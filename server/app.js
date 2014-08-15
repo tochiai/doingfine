6021a6d0839932f6d901fcaac8cfd3347b640d28
Tyler Ochiai <tyler.a.ochiai@gmail.com>
2014-08-14 19:34:48 -0700

    (bugfix) Allow cors

--- server/app.js
+++ server/app.js
@@ -11,6 +11,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 var config = require('./config/environment');
 var cron = require('./components/cron/cron.js');
+var cors = require('express-cors');
 
 // Connect to database
 mongoose.connect(config.mongo.uri, config.mongo.options);
@@ -18,8 +19,15 @@ mongoose.connect(config.mongo.uri, config.mongo.options);
 // Populate DB with sample data
 if(config.seedDB) { require('./config/seed'); }
 
-// Setup server
+// Allow cors
 var app = express();
+app.use(cors({
+    allowedOrigins: [
+        'localhost:8100'
+    ]
+}));
+
+// Setup server
 var server = require('http').createServer(app);
 var socketio = require('socket.io').listen(server);
 require('./config/socketio')(socketio);
