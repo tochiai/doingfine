6b2ed4798ae09f8e0d84181147f18626fdcd12d6
Walker Mellema <walkermellema@gmail.com>
2014-08-16 14:53:27 -0700

    connect form to model properly

--- client/app/account/settings/settings.controller.js
+++ client/app/account/settings/settings.controller.js
@@ -21,11 +21,11 @@ angular.module('doingFineApp')
 
     var getUser = function() {
       User.get(function(result){
-        $scope.userID = result._id;
-        $scope.userName = result.name;
-        $scope.phone = result.phone.slice(2);
-        $scope.email = result.email;
-        console.log(result);
+        $scope.user = result;
+        //$scope.userName = result.name;
+        $scope.user.phone = result.phone.slice(2);
+        //$scope.email = result.email;
+        //console.log(result);
         // $http.get('api/users/' + $scope.userID + '/schedules')
         //   .success(function(data){
         //     $scope.mySchedules = data;
